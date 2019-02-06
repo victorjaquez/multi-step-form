34f7ab127615c5c355f9f945016b9e08b0f811b2
victorjaquez <victor@jaquez.me>
2019-02-06 14:31:08 -0700

    form user details step and material ui styling

--- src/Components/FormUserDetails.js
+++ src/Components/FormUserDetails.js
@@ -1,11 +1,56 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+import TextField from "material-ui/TextField";
+import RaisedButton from "material-ui/RaisedButton";
 
 export default class FormUserDetails extends Component {
+  continue = e => {
+    e.preventDefault();
+    this.props.nextStep();
+  };
+
   render() {
+    const { values, handleChange } = this.props;
     return (
-      <div>
-        <h1>Form User Details</h1>
-      </div>
+      <MuiThemeProvider>
+        <Fragment>
+          <AppBar title="Enter User Details" />
+          <TextField
+            hintText="Enter Your First Name"
+            floatingLabelText="First Name"
+            onChange={handleChange("firstName")}
+            defaultValue={values.firstName}
+          />
+          <br />
+          <TextField
+            hintText="Enter Your Last Name"
+            floatingLabelText="Last Name"
+            onChange={handleChange("lastName")}
+            defaultValue={values.lastName}
+          />
+          <br />
+          <TextField
+            hintText="Enter Your Email"
+            floatingLabelText="Email"
+            onChange={handleChange("email")}
+            defaultValue={values.email}
+          />
+          <br />
+          <RaisedButton
+            label="Continue"
+            primary={true}
+            style={styles.button}
+            onClick={this.continue}
+          />
+        </Fragment>
+      </MuiThemeProvider>
     );
   }
 }
+
+const styles = {
+  button: {
+    margin: 15
+  }
+};
