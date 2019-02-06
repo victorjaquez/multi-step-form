ac3ec31f54b50451c909abebfc871f82856d6f1b
victorjaquez <victor@jaquez.me>
2019-02-06 14:03:14 -0700

    component set up, initial state & methods, step 1 of form

---
+++ src/Components/FormUserDetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class FormUserDetails extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Form User Details</h1>
+      </div>
+    );
+  }
+}
