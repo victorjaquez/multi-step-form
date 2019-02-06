bdb7949b7788e46203df0f74478feaf7440aa3d7
victorjaquez <victor@jaquez.me>
2019-02-06 14:54:25 -0700

    confirm and submit logic step

---
+++ src/Components/Confirm.js
@@ -0,0 +1,56 @@
+import React, { Component, Fragment } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+import { List, ListItem } from "material-ui/List";
+import RaisedButton from "material-ui/RaisedButton";
+
+export default class Confirm extends Component {
+  continue = e => {
+    e.preventDefault();
+    // process form, for future api integration
+    this.props.nextStep();
+  };
+
+  back = e => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
+
+  render() {
+    const {
+      values: { firstName, lastName, email, occupation, city, bio }
+    } = this.props;
+    return (
+      <MuiThemeProvider>
+        <Fragment>
+          <AppBar title="Confirm User Data" />
+          <List>
+            <ListItem primaryText="First Name" secondaryText={firstName} />
+            <ListItem primaryText="Last Name" secondaryText={lastName} />
+            <ListItem primaryText="Email" secondaryText={email} />
+            <ListItem primaryText="Occupation" secondaryText={occupation} />
+            <ListItem primaryText="Bio" secondaryText={bio} />
+          </List>
+          <RaisedButton
+            label="Confirm and Continue"
+            primary={true}
+            style={styles.button}
+            onClick={this.continue}
+          />
+          <RaisedButton
+            label="Back"
+            primary={false}
+            style={styles.button}
+            onClick={this.back}
+          />
+        </Fragment>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+const styles = {
+  button: {
+    margin: 15
+  }
+};
--- src/Components/UserForm.js
+++ src/Components/UserForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import FormUserDetails from "./FormUserDetails";
 import FormPersonalDetails from "./FormPersonalDetails";
+import Confirm from "./Confirm";
 
 export default class UserForm extends Component {
   state = {
@@ -58,7 +59,13 @@ export default class UserForm extends Component {
           />
         );
       case 3:
-        return <h1>Confirm</h1>;
+        return (
+          <Confirm
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            values={values}
+          />
+        );
       case 4:
         return <h1>Success</h1>;
     }
